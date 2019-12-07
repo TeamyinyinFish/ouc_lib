2996d597a5bafba99fbe7c3bd3903bdb06db0d34
S624122050 <44426652+S624122050@users.noreply.github.com>
2019-12-07 14:02:59 +0800

    Delete dbutil.py

--- dbutil.py
+++
@@ -1,6 +0,0 @@
-import pymysql
-
-def connect():
-    db = pymysql.connect(host='localhost', user='root', password = 'root', port = 3306, db = 'comment')
-    cursor = db.cursor()
-    return db, cursor
